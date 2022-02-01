71b74b8a0e2bbc1b880f640cb7a34badcb5b3edd
Farhan Masud Aneek <farhan.aneek@gmail.com>
2022-02-02 00:33:58 +0600

    Django browser reload for tailwind bug fixed - added on local settings

--- config/settings/local.py
+++ config/settings/local.py
@@ -7,7 +7,11 @@ ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 
 # whitenoise and django debug toolbar for development
-INSTALLED_APPS = ["whitenoise.runserver_nostatic"] + INSTALLED_APPS + ["debug_toolbar"]
+INSTALLED_APPS = (
+    ["whitenoise.runserver_nostatic"]
+    + INSTALLED_APPS
+    + ["debug_toolbar", "django_browser_reload"]
+)
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
