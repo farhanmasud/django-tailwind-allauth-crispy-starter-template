f791a684a48381ef1eef7cbf463c5400348e069e
Farhan Masud Aneek <farhan.aneek@gmail.com>
2022-03-12 15:48:39 +0600

    Sentry settings moved to staging and production from base

--- config/settings/base.py
+++ config/settings/base.py
@@ -190,24 +190,3 @@ ACCOUNT_UNIQUE_EMAIL = True
 ACCOUNT_AUTHENTICATION_METHOD = "email"
 ACCOUNT_USERNAME_REQUIRED = False
 ACCOUNT_USER_MODEL_USERNAME_FIELD = None
-
-
-import sentry_sdk
-from sentry_sdk.integrations.django import DjangoIntegration
-
-# try for sentry variable on the .env file
-
-try:
-    sentry_sdk.init(
-        dsn=env("SENTRY_DSN"),
-        integrations=[DjangoIntegration()],
-        # Set traces_sample_rate to 1.0 to capture 100%
-        # of transactions for performance monitoring.
-        # We recommend adjusting this value in production.
-        traces_sample_rate=1.0,
-        # If you wish to associate users to errors (assuming you are using
-        # django.contrib.auth) you may enable sending PII data.
-        send_default_pii=True,
-    )
-except:
-    pass
--- config/settings/production.py
+++ config/settings/production.py
@@ -10,3 +10,24 @@ MIDDLEWARE += (
     "django.middleware.common.CommonMiddleware",
     "django.middleware.cache.FetchFromCacheMiddleware",
 )
+
+
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+# try for sentry variable on the .env file
+
+try:
+    sentry_sdk.init(
+        dsn=env("SENTRY_DSN"),
+        integrations=[DjangoIntegration()],
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        # We recommend adjusting this value in production.
+        traces_sample_rate=1.0,
+        # If you wish to associate users to errors (assuming you are using
+        # django.contrib.auth) you may enable sending PII data.
+        send_default_pii=True,
+    )
+except:
+    pass
