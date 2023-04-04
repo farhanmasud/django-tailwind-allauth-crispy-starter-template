20e43b4dd6cf940ef550e9018db107b5ee95d9c4
Farhan Masud Aneek <farhan.aneek@gmail.com>
2023-04-04 07:47:27 +0600

    Upgraded requirements to latest | Django 4.2

--- accounts/models.py
+++ accounts/models.py
@@ -3,7 +3,6 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.base_user import BaseUserManager
-from django.utils.translation import ugettext_lazy as _
 
 
 class CustomUserManager(BaseUserManager):
