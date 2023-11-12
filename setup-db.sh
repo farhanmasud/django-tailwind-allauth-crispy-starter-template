8c618c7c10666331337671556bdbdfe20d3090c2
Farhan M <farhan.aneek@gmail.com>
2023-11-12 20:28:04 +0600

    PGPASSWORD added and utilized in setup-db.bash script

--- setup-db.sh
+++ setup-db.sh
@@ -6,9 +6,9 @@ fi
 
 cd ~postgres
 
-sudo -u postgres psql -c "CREATE DATABASE $DB_NAME;"
-sudo -u postgres psql -c "CREATE USER $DB_USER WITH PASSWORD '$DB_PASSWORD';"
-sudo -u postgres psql -c "ALTER ROLE $DB_USER SET client_encoding TO 'utf8';"
-sudo -u postgres psql -c "ALTER ROLE $DB_USER SET default_transaction_isolation TO 'read committed';"
-sudo -u postgres psql -c "ALTER ROLE $DB_USER SET timezone TO 'UTC';"
-sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO $DB_USER;"
+sudo PGPASSWORD=$PGPASSWORD -u postgres psql -c "CREATE DATABASE $DB_NAME;"
+sudo PGPASSWORD=$PGPASSWORD -u postgres psql -c "CREATE USER $DB_USER WITH PASSWORD '$DB_PASSWORD';"
+sudo PGPASSWORD=$PGPASSWORD -u postgres psql -c "ALTER ROLE $DB_USER SET client_encoding TO 'utf8';"
+sudo PGPASSWORD=$PGPASSWORD -u postgres psql -c "ALTER ROLE $DB_USER SET default_transaction_isolation TO 'read committed';"
+sudo PGPASSWORD=$PGPASSWORD -u postgres psql -c "ALTER ROLE $DB_USER SET timezone TO 'UTC';"
+sudo PGPASSWORD=$PGPASSWORD -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO $DB_USER;"
